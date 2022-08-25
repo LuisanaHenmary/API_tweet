22680891a46a8d58c157ead8cc911f7c48addb74
Luisana Perez <luisanahenmaryp@gmail.com>
2022-08-25 10:55:07 -0400

    Created the user model

---
+++ models/user.py
@@ -0,0 +1,52 @@
+#python native
+from uuid import UUID
+from datetime import date
+from typing import Optional
+
+#pydantic
+from pydantic import (
+    BaseModel,
+    EmailStr,
+    Field
+)
+
+class UserBase(BaseModel):
+    user_id: UUID  = Field(...)
+
+    email: EmailStr = Field(
+        ...,
+        example='sebas@sebas.com'
+    )
+
+class User(UserBase):
+    user_name: str = Field(
+        ...,
+        min_length=3,
+        max_length=10,
+        example='sgewux'
+    )
+
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=30
+    )
+
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=30
+    )
+
+    birth_date: Optional[date] = Field(
+        default=None
+    )
+
+
+class UserLogin(UserBase):
+    password: str = Field(
+        ...,
+        example="noUseThis00",
+        min_length=8,
+        max_length=64
+    )
